0a61d1f4ee5aa1cd5e76d0dce9f2558305d55d33
Rafael Fernandes <Rafaelsf013@gmail.com>
2021-01-10 12:08:14 -0300

    Tipos de dados

---
+++ tipoDados.js
@@ -0,0 +1,18 @@
+console.log(`Rafael ${1+1}`)
+
+console.log(12.5 === Infinity)
+
+console.log(null === undefined)
+
+console.log({
+
+    name:'Rafael Santos Fernandes',
+    company: 'SpaceX',
+    group: 'C++ Developer',
+    programar:() => console.log('Programando...')
+
+})
+
+console.log(['Rafael', 40, [1,2,3], 'São Vicente/SP'])
+
+console.log(null === {})
